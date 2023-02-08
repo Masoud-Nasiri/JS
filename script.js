f69e555cd005d2aed88e92b32d5fa7caa073c13f
Masoud Nasiri <35027884+Masoud-Nasiri@users.noreply.github.com>
2023-02-08 22:10:12 +0330

    Test

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const day = 'Monday';
 
+switch (day) {
+    case 'Monday':
+        console.log('Start trading.');
+        break;
+    case 'Tuesday':
+        console.log('Start Tuesday.');
+        break;
+    case 'Wednesday':
+    case 'Thursday':
+        console.log('Trading and go to work.');
+        break;
+    case 'Friday':
+        console.log('Have a weekends.');
+        break;
+    case 'Saturday':
+    case 'Sunday':
+        console.log('Do no trade');
+        console.log('Just go to work');
+        break;
+    default:
+        console.log('No valid day.')
+}
